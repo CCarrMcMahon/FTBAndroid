1002006385f03ac75aaba985d3a6d9329621605e
Christopher McMahon <CCarrMcMahon@gmail.com>
2022-03-14 01:33:24 -0500

    Finally have Bluetooth connection!
    This commit is the first one that establishes an actual Bluetooth connection. After modifying the code to work with BLE devices, I had to create a service to handle the GATT server. This initially didn't work, however, I finally realized we need to define the service in the Manifest so the program knows what it is.
    
    From here, I can start learning how to communicate and transfer data. That means the next step is to transfer the Wi-Fi credentials.

--- app/src/main/java/com/example/feedthebeast/WiFiDetails.java
+++ app/src/main/java/com/example/feedthebeast/WiFiDetails.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -20,6 +28,8 @@ import java.util.Objects;
 // Also adjust RecyclerView so arguments can be passed in that allow a single use adapter
 // This could be done by passing in what view to get and what row to use
 public class WiFiDetails extends AppCompatActivity {
+    private final static String TAG = WiFiDetails.class.getSimpleName();
+
     private BluetoothDevice bluetoothDevice;
     private Toolbar tb;
     private TextInputEditText tiet_SSID;
@@ -27,6 +37,56 @@ public class WiFiDetails extends AppCompatActivity {
     private ProgressBar pb;
     private Button btn_connect;
     private TextView tv_WiFiInfo;
+
+    private final Context context = this;
+
+    private BluetoothLeService bluetoothLeService;
+    private String deviceAddress;
+    private boolean connected = false;
+
+    private final ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            bluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
+            if (!bluetoothLeService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+            // Automatically connects to the device upon successful start-up initialization.
+            bluetoothLeService.connect(deviceAddress);
+            Log.d(TAG, "Service Connected");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            bluetoothLeService = null;
+            Log.d(TAG, "Service Disconnected");
+        }
+    };
+
+    private final BroadcastReceiver gattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (BluetoothLeService.ACTION_GATT_CONNECTED.equals(action)) {
+                connected = true;
+                tv_WiFiInfo.append("\nGATT Connected\n");
+                Log.d(TAG, "GATT Connected");
+            } else if (BluetoothLeService.ACTION_GATT_DISCONNECTED.equals(action)) {
+                connected = false;
+                tv_WiFiInfo.append("\nGATT Disconnected\n");
+                Log.d(TAG, "GATT Disconnected");
+            }
+        }
+    };
+
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_DISCONNECTED);
+        return intentFilter;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,20 +119,38 @@ public class WiFiDetails extends AppCompatActivity {
         }
 
         String btName = bluetoothDevice.getName();
-        String btMAC = bluetoothDevice.getAddress();
+        deviceAddress = bluetoothDevice.getAddress();
 
         tv_WiFiInfo.setText(String.format("Bluetooth Device:\nName: %s\nMAC: %s\n\n" +
-                "Wi-Fi:\nSSID: %s\nIP: %s\nMAC: %s\nBSSID: %s\nRSSI: %s", btName, btMAC,
+                "Wi-Fi:\nSSID: %s\nIP: %s\nMAC: %s\nBSSID: %s\nRSSI: %s", btName, deviceAddress,
                 ssid, ip, mac, bssid, rssi));
+
+        Intent gattServiceIntent = new Intent(context, BluetoothLeService.class);
+        bindService(gattServiceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
 
         btn_connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // BTConnectThread btConnectThread = new BTConnectThread(bluetoothDevice);
-                // btConnectThread.start();
-
                 pb.setVisibility(View.VISIBLE);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        registerReceiver(gattUpdateReceiver, makeGattUpdateIntentFilter());
+
+        if (bluetoothLeService != null) {
+            final boolean result = bluetoothLeService.connect(deviceAddress);
+            Log.d(TAG, "Connect request result=" + result);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(gattUpdateReceiver);
     }
 }
